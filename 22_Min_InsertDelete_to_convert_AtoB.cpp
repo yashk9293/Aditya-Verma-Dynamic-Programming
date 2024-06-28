1f536fe3a24bf786e5ebc46c5be0ef4bd5794729
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-28 19:58:16 +0530

    Update 22_Min_InsertDelete_to_convert_AtoB.cpp

--- 22_Min_InsertDelete_to_convert_AtoB.cpp
+++ 22_Min_InsertDelete_to_convert_AtoB.cpp
@@ -1,39 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Question Link :- https://www.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1
+// Minimum number of deletions and insertions
 
-int LCS(string X, string Y, int n, int m) {
-	int dp[n + 1][m + 1]; // DP - matrix
-
-	// base case of recursion --> for initialization of dp - matrix
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
-
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if (X[i - 1] == Y[j - 1]) // when last character is same
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else // when last character is not same -> pick max
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-
-	return dp[n][m];
-}
-
-void MinInsertDel(string X, string Y, int n, int m) {
-	int lcs_len = LCS(X, Y, n, m);
-	cout << "Minimum number of deletions = " << (n - lcs_len)<<endl;
-    cout << "Minimum number of insertions = " << (m - lcs_len)<<endl;
-}
-
-int main() {
-	string X, Y;
-    cin >> X >> Y;
-	int n = X.length(), m = Y.length();
-
-	MinInsertDel(X, Y, n, m);
-	return 0;
-}
+// T.C = O(n*m)
+// S.C = O(n*m)
+class Solution {
+	public:
+	int LCS(string X, string Y, int m, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (Y[i - 1] == X[j - 1]) { 
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    
+	int minOperations(string X, string Y) {
+	    int m = X.length(), n = Y.length();
+	    int deletions = m - LCS(X, Y, m, n);
+	    int insertions = n - LCS(X, Y, m, n);
+	    
+	    return deletions + insertions;
+	}
+};
 
 
 // Input :-
@@ -41,5 +34,4 @@ int main() {
 // pea
 
 // Output :-
-// Minimum number of deletions = 2
-// Minimum number of insertions = 1
+// 3 (deletions = 2 + insertions = 1)
