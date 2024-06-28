5f319da223b7a564e4bbdb085d55e7e479daee2c
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-28 19:58:39 +0530

    Update 21_Shortest_Common_Supersequence.cpp

--- 21_Shortest_Common_Supersequence.cpp
+++ 21_Shortest_Common_Supersequence.cpp
@@ -1,33 +1,57 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Question Link :- https://www.geeksforgeeks.org/problems/shortest-common-supersequence0322/1
+// Shortest Common Supersequence
 
-int LCS(string X, string Y, int n, int m) {
-	int dp[n + 1][m + 1];
+// T.C = O(m*n)
+// S.C = O(m*n)
 
-	// base case of recursion --> for initialization of dp - matrix
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
+// Approach - 1 (Memoization)
+class Solution {
+    public:
+    int t[101][101];
+    int LCS(string X, string Y, int m, int n) {
+    	// base case
+    	if (m == 0 || n == 0) {
+    		return 0;
+        }
+    	if (t[m][n] != -1) {
+    		return t[m][n];
+        }
+        // choice diagram
+    	if (X[m - 1] == Y[n - 1]) {    // when last character is same
+    		t[m][n] = 1 + LCS(X, Y, m - 1, n - 1);    // count the number and decreament the both's string length // store the value in particular block 
+        } 
+        else {   // when last character is not same -> pick max
+    		t[m][n] = max(LCS(X, Y, m - 1, n), LCS(X, Y, m, n - 1)); // one take full and another by leaving last char and vice versa // store the value in particular block 
+        }
+    	return t[m][n];
+    }
+    
+    int shortestCommonSupersequence(string X, string Y, int m, int n) {
+        memset(t, -1, sizeof(t));
+        return m + n - LCS(X, Y, m, n);
+    }
+};
 
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if (X[i - 1] == Y[j - 1])
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 
-	return dp[n][m];
-}
 
-int SCS(string X, string Y, int n, int m) {
-	return m + n - LCS(X, Y, n, m);              // formula // n-> length of string x ; m-> length of string y
-}
-
-signed main() {
-	string X, Y; cin >> X >> Y;
-	int n = X.length(), m = Y.length();
-
-	cout << SCS(X, Y, n, m) << endl;
-	return 0;
-}
+// Approach - 2 (Tabulation)
+class Solution {
+    public:
+    int LCS(string X, string Y, int m, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (Y[i - 1] == X[j - 1]) { 
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    
+    int shortestCommonSupersequence(string X, string Y, int m, int n) {
+        return m + n - LCS(X, Y, m, n);
+    }
+};
