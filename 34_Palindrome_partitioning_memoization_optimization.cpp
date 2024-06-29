6fe423972342146e0da602d249ff1ce1ba9c3a97
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-30 00:44:52 +0530

    Update 34_Palindrome_partitioning_memoization_optimization.cpp

--- 34_Palindrome_partitioning_memoization_optimization.cpp
+++ 34_Palindrome_partitioning_memoization_optimization.cpp
@@ -1,50 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Question Link :- https://leetcode.com/problems/palindrome-partitioning-ii/
+// Palindrome Partitioning II
 
-const int D = 1001;
-int t[D][D];
-
-bool isPallindrome(string X, int i, int j) {
-	while (i <= j) {
-		if (X[i] != X[j]) {
-			return false;
+// Memoization Optimized (striver)
+// T.C = O(N^2) There are a total of N states and inside each state, a loop of size N(apparently) is running.
+// S>C = O(N) ->[dp array] + Auxiliary stack space O(N)
+class Solution {
+public:
+    bool isPalindrome(string& X, int i, int j) {  // Note:- pass string by reference, to avoid TLE
+        while (i <= j) {
+            if (X[i] != X[j]) {
+                return false;
+            }
+            i++, j--;
         }
-		i++, j--;
-	}
-	return true;
-}
+        return true;
+    }
 
-int Solve(string X, int i, int j) {
-	if (t[i][j] != -1)
-		return t[i][j];
-
-	if (i >= j || isPallindrome(X, i, j)) {
-		t[i][j] = 0;
-		return 0;
-	}
-
-	int ans = INT_MAX;
-	for (int k = i; k < j; k++) {
-
-		int left, right;
-		if (t[i][k] != -1) {
-			left = t[i][k];
+    int Solve(string &str, int i, int j, vector<int> &t) {
+        // Base case: If we've reached the end of the string.
+        if (i >= j) {
+            return 0;
         }
-		else {
-			left = Solve(X, i, k);
+        if (t[i] != -1) {
+            return t[i];
         }
-
-
-		if (t[k + 1][j] != -1) {
-			right = t[k + 1][j];
+        int ans = INT_MAX;
+        // Consider all possible substrings starting from the current index.
+        for (int k = i; k <= j-1; k++) {
+            if (isPalindrome(str, i, k)) {
+                // If the substring is a palindrome, calculate the cost and minimize it.
+                int temp_ans = 1 + Solve(str, k + 1, j, t);
+                ans = min(ans, temp_ans);
+            }
         }
-		else {
-			right = Solve(X, k + 1, j);
-        }
+        return t[i] = ans;
+    }
 
-		int temp_ans = left + right + 1;
-		ans = min(ans, temp_ans);
-	}
-
-	return t[i][j] = ans;
-}
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> t(n, -1);
+        return Solve(s, 0, n, t) - 1;
+    }
+};
