91930a2313d9706ac429a734a0472cc4aaf7c2f6
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-30 00:44:30 +0530

    Update 33_Palindrome_Partitioning_memoization.cpp

--- 33_Palindrome_Partitioning_memoization.cpp
+++ 33_Palindrome_Partitioning_memoization.cpp
@@ -1,42 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Question Link :- https://leetcode.com/problems/palindrome-partitioning-ii/
+// Palindrome Partitioning II
 
-const int D = 1001;
-int t[D][D];
-
-bool isPallindrome(string X, int i, int j) {
-	while (i <= j) {
-		if (X[i] != X[j]) {
-			return false;
+// Memoization (giving TLE)
+class Solution {
+public:
+    bool isPalindrome(string X, int i, int j) { // function to check either a string is palindrome or not 
+        while (i <= j) {
+            if (X[i] != X[j]) {
+                return false;
+            }
+            i++, j--;
         }
-		i++, j--;
-	}
-	return true;
-}
+        return true;
+    }
 
-int Solve(string X, int i, int j) {
-	if (i >= j || isPallindrome(X, i, j)) {
-		t[i][j] = 0;
-		return 0;
-	}
+    int Solve(string X, int i, int j, vector<vector<int>>& t) {
+        if (i >= j || isPalindrome(X, i, j)) {
+            t[i][j] = 0;
+            return 0;
+        }
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
+        int ans = INT_MAX;
+        for(int k = i; k <= j-1; k++) {
+            int temp_ans = Solve(X, i, k, t) + Solve(X, k + 1, j, t) + 1;
+            ans = min(ans, temp_ans);
+        }
+        return t[i][j] = ans;
+    }
 
-	if (t[i][j] != -1)
-		return t[i][j];
-
-	int ans = INT_MAX;
-	for (int k = i; k < j; k++) {
-		int temp_ans = Solve(X, i, k) + Solve(X, k + 1, j) + 1;
-		ans = min(ans, temp_ans);
-	}
-
-	return t[i][j] = ans;
-}
-
-int main() {
-	string X;
-    cin >> X;
-	memset(t, -1, sizeof(t));
-
-	cout << Solve(X, 0, X.length() - 1) << endl;      // Note : here we have to pass i as 0, because we have to check from starting
-	return 0;
-}
+    int minCut(string s) {
+        int n = s.length();
+        vector<vector<int>> t(n, vector<int>(n, -1));
+        return Solve(s, 0, n-1, t);
+    }
+};
