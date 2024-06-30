dbf350d23273946401a8408ecbd5cf23ef5af391
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-30 21:36:53 +0530

    Update and rename 43_MaxPathSum.cpp to 44_MaxPathSum.cpp

--- 44_MaxPathSum.cpp
+++ 44_MaxPathSum.cpp
@@ -4,25 +4,24 @@
 
 // T.C = O(n)
 // S.C = O(n)
-
 int solve(TreeNode* root,int& res) {
-    if(root==nullptr) {
+    if(root == nullptr) {
         return 0;
     }
     int l = solve(root->left, res);
     int r = solve(root->right, res);
     
-    int temp=max( max(l,r) + root->val, root->val);
-    int ans = max(temp, l+r+root->val);
-    res=max(res,ans);
+    int temp=max(root->val + max(l,r), root->val); // if l or r is giving -ve value then take only root->val
+    int ans = max(temp, l + r + root->val);
+    res = max(res,ans);
     
     return temp;
 }
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-    int res=INT_MIN;
-    solve(root,res);
+    int res = INT_MIN;
+    solve(root, res);
     return res; 
     }
 };
