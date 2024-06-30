bcd4368817425a014f2dc6cc1b162010d2296636
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2024-06-30 20:56:42 +0530

    Update and rename 39_EggDropping_recursive.cpp to 40_EggDropping_recursive.cpp

--- 40_EggDropping_recursive.cpp
+++ 40_EggDropping_recursive.cpp
@@ -1,12 +1,16 @@
+// T.C = O(floor * (2^min(floor, egg))
+// S.C = O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int Solve(int eggs, int floors) {
-	if (eggs == 1)
+	if (eggs == 0) {
+		return 0;
+	}
+	if (eggs == 1 || floors == 0 || floors == 1) {
 		return floors;
-	if (floors == 0 || floors == 1)
-		return floors;
-
+	}
 	int mn = INT_MAX;
 	for (int k = 1; k <= floors; k++) {
 		int temp_ans = 1 + max(Solve(eggs - 1, k - 1), Solve(eggs, floors - k)); // once egg break means decrement both floor and egg another agg did not break means check egg dropping from above 
