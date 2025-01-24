dcdddcadb17f366afd272b86468b363e024314a6
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2025-01-25 02:21:13 +0530

    Update 07_Minimum_subset_sum_difference.cpp

--- 07_Minimum_subset_sum_difference.cpp
+++ 07_Minimum_subset_sum_difference.cpp
@@ -3,6 +3,8 @@
 
 // Note :- 0 < arr[i] <= 10^5
 
+
+// Tabulation
 // T.C = O(n*sum)
 // S.C = O(n*sum)
 class Solution {
@@ -51,4 +53,43 @@ public:
         }
         return mn;
 	} 
+};
+
+
+
+// Memoization
+// T.C = O(n*sum)
+// S.C = O(n*sum)
+class Solution {
+  public:
+    int solve(vector<int>& arr, int n, int sum, int currSum, vector<vector<int>>& dp) {
+        int ans=INT_MAX;
+        if(n == 0) {
+            ans = min(ans, abs(sum - 2*currSum));
+            return ans;
+        }
+        if(n < 0) {
+            return 0;
+        }
+        
+        if(dp[n][currSum] != -1) {
+            return dp[n][currSum];
+        }
+        
+        int take = 0, skip = 0;
+        take = solve(arr, n-1, sum, currSum + arr[n-1], dp);
+        skip = solve(arr, n-1, sum, currSum, dp);
+        
+        return dp[n][currSum] = min(take, skip);
+    }
+
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;
+	    for(int i=0; i<n; i++){
+	        sum += arr[i];
+	    }
+	    vector<vector<int>>dp(n+1, vector<int>(sum+1, -1));
+	    return solve(arr, n, sum, 0, dp);
+    }
 };
