fd3662328a26e34ca1b1c03bdf1dca836a85c240
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2025-01-25 02:18:49 +0530

    Update 01_Recursive_Knapsack.cpp

--- 01_Recursive_Knapsack.cpp
+++ 01_Recursive_Knapsack.cpp
@@ -3,31 +3,33 @@ using namespace std;
 
 int Knapsack(int wt[], int val[], int W, int n) {
 	// every recursive solution will have a base condition 
-    // for base condition we need to think of the smallest valid input that we can pass 
-    // array size can be atleast 0 || min weight can be 0 but not negetive; 
-	if (n == 0 || W == 0)
+        // for base condition we need to think of the smallest valid input that we can pass 
+        // array size can be atleast 0 || min weight can be 0 but not negetive; 
+	if (n == 0 || W == 0) {
 		return 0;
-
+	}
 	// these are the choices we are having 
 	if (wt[n - 1] <= W) {
 		return max(val[n - 1] + Knapsack(wt, val, W - wt[n - 1], n - 1), Knapsack(wt, val, W, n - 1));
 	}
-	else if (wt[n - 1] > W) // if the weight is greater then the required weight there is no sence for taking that value. 
-		return Knapsack(wt, val, W, n - 1); // return as it is by redusing the size of array 
-	else
+	else if (wt[n - 1] > W) { // if the weight is greater then the required weight there is no sence for taking that value. 
+		return Knapsack(wt, val, W, n - 1); // return as it is by reducing the size of array 
+	} else {
 		return -1; 
+	}
 }
 
 int main() {
-	int n,W; 
-    cin >> n; // number of items
+	int n, W; 
+        cin >> n; // number of items
 	int val[n], wt[n]; // values and weights of array
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) {
 		cin >> wt[i];
-	for (int i = 0; i < n; i++)
+	}
+	for (int i = 0; i < n; i++) {
 		cin >> val[i];
-	
-    cin >> W; // Knapsack capacity
+	}
+        cin >> W; // Knapsack capacity
 
 	cout << Knapsack(wt, val, W, n) << endl;
 	return 0;
